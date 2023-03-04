74c422cec09eef1683f089c4ea0b88ce3494a3f7
douglas <38635101+dougdbp@users.noreply.github.com>
2023-03-03 21:27:25 -0300

    Creae Logout button-icon. Set Button and ButtonText components style.

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,13 +1,31 @@
-import { Container } from "./styles";
+import { Container, Links } from "./styles";
 
 import { Header } from "../../components/Header";
-import { Button } from "../../components/button";
+import { Button } from "../../components/Button";
+import { Section } from "../../components/Section";
+import { Tag } from "../../components/Tag";
+import { ButtonText } from "../../components/ButtonText";
 
 export function Details(){
 
   return(
     <Container>
       <Header />
+
+      <ButtonText title="Excluir Nota" />
+
+      <Section title="Links úteis">
+        <Links>
+          <li><a href="#">https://rocketseat.com.br</a></li>
+          <li><a href="#">https://rocketseat.com.br</a></li>
+        </Links>
+      </Section>
+
+      <Section title="Marcadores">
+        <Tag title="Express"/>
+        <Tag title="Node" />
+      </Section>
+
       <Button title="Voltar" />
     </Container>
   )

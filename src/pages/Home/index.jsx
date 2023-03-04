f120a4e23d4239b15f4af5d9173b31a9f5590973
douglas <38635101+dougdbp@users.noreply.github.com>
2023-03-04 16:57:43 -0300

    page home finished. Notes Components done.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,11 @@
-import { FiPlus } from 'react-icons/fi';
-import { FiSearch} from 'react-icons/fi';
+import { FiPlus, FiSearch } from 'react-icons/fi';
 
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles';
 
-import { Header } from '../../components/Header';
+import { Note } from '../../components/Note';
 import { Input } from '../../components/Input';
-
+import { Header } from '../../components/Header';
+import { Section } from '../../components/Section';
 import { ButtonText } from '../../components/ButtonText';
 
 export function Home() {
@@ -28,7 +28,16 @@ export function Home() {
             </Search>
 
             <Content>
-
+                <Section title="Minhas Notas">
+                    <Note data={{
+                        title:'React', 
+                        tags: [
+                            { id: '1', name: 'react'},
+                            { id: '2', name: 'rocketseat'}
+                        ]
+                    }}
+                    />
+                </Section>
             </Content>
 
             <NewNote>

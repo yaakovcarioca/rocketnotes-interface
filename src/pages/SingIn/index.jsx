12a6dd45c86f4683f378c1a51c34b7ad9424b257
douglas <38635101+dougdbp@users.noreply.github.com>
2023-03-06 10:13:11 -0300

    Page SingIn's code finished

--- src/pages/SingIn/index.jsx
+++ src/pages/SingIn/index.jsx
@@ -1,9 +1,36 @@
-import { Container } from "./styles";
+import { Container, Form } from "./styles";
+import { FiMail, FiLock } from 'react-icons/fi';
+import { Input } from '../../components/Input';
+import { Button } from '../../components/Button';
 
 export function SingIn() {
     return (
         <Container>
+            <Form>
+                <h1>Rocket Notes</h1>
+                <p>Aplicação para salvar e gerenciar seus links úteis.</p>
 
+                <h2>Faça seu login</h2>
+
+                <Input 
+                    placeholder="E-mail"
+                    type="text"
+                    icon={FiMail}
+                />
+                
+                <Input 
+                    placeholder="E-mail"
+                    type="text"
+                    icon={FiLock}
+                />
+
+                <Button title="Entrar" />
+
+                <a href="#">
+                    Criar conta
+                </a>
+
+            </Form>
         </Container>
     );
 }

bb296e5fc31232f45e72203417bb053dac3ac86c
douglas <38635101+dougdbp@users.noreply.github.com>
2023-03-06 12:06:47 -0300

    New note page's header created

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,11 +1,25 @@
 import { Header } from '../../components/Header';
+import { Input } from '../../components/Input';
 
-import { Container } from "./style";
+import { Container, Form } from "./style";
 
 export function New() {
     return (
         <Container>
             <Header />
+
+            <main>
+                <Form>
+                    <header>
+                        <h1>Criar nota</h1>
+                        <a href="/">voltar</a>
+                    </header>
+
+                    <Input
+                    placeholder="Título"
+                    />
+                </Form>
+            </main>
         </Container>
     );
 };

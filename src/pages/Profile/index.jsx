27c202bfc900ea4cf065f043324c3280ee8946cc
douglas <38635101+dougdbp@users.noreply.github.com>
2023-03-06 11:48:43 -0300

    Finished Profile page & profile user image

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,6 +1,6 @@
-import { Container, Form } from './styles';
+import { Container, Form, Avatar } from './styles';
 
-import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi'
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi'
 
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
@@ -16,6 +16,22 @@ export function Profile() {
             </header>
 
             <Form>
+                <Avatar>
+                    <img 
+                        src="https://github.com/dougdbp.png" 
+                        alt="Foto do usuário" 
+                    />
+
+                    <label htmlFor="avatar">
+                        <FiCamera />
+
+                        <input
+                            id="avatar"
+                            type="file"
+                        />
+                    </label>
+                </Avatar>
+
                 <Input
                     placeholder="Nome"
                     type="text"

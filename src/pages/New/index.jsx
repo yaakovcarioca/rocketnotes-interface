70c291f9acb9c060558f25788f88e31d994472e6
douglas <38635101+dougdbp@users.noreply.github.com>
2023-03-06 11:59:44 -0300

    New note page's interface created

---
+++ src/pages/New/index.jsx
@@ -0,0 +1,11 @@
+import { Header } from '../../components/Header';
+
+import { Container } from "./style";
+
+export function New() {
+    return (
+        <Container>
+            <Header />
+        </Container>
+    );
+};

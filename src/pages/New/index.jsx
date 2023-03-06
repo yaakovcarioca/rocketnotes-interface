1279b36d71f6565f53821497cc9a7eeedf8f541b
douglas <38635101+dougdbp@users.noreply.github.com>
2023-03-06 16:36:00 -0300

    New note page's NoItem component created

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -2,7 +2,7 @@ import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
 import { Textarea } from '../../components/Textarea';
 
-import { Container, Form } from "./style";
+import { Container, Form } from "./styles";
 
 export function New() {
     return (

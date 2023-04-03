cc8cede92b3cd86f8bdbb542977eb5d5af74f800
douglas <38635101+dougdbp@users.noreply.github.com>
2023-04-03 10:27:35 -0300

    resolving routes bug.

--- src/pages/SingUp/index.jsx
+++ src/pages/SingUp/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Container, Form, Background } from "./styles";
 import { FiMail, FiLock, FiUser } from 'react-icons/fi';
 import { Input } from '../../components/Input';
@@ -6,6 +7,14 @@ import { Button } from '../../components/Button';
 import { Link } from "react-router-dom";
 
 export function SingUp() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    function handleSingUp(){
+        console.log(name, email, password)
+    }
+
     return (
         <Container>
 
@@ -22,21 +31,24 @@ export function SingUp() {
                     placeholder="Nome"
                     type="text"
                     icon={FiUser}
+                    onChange={e => setName(e.target.value)}
                 />
 
                 <Input 
                     placeholder="E-mail"
                     type="text"
                     icon={FiMail}
+                    onChange={e => setEmail(e.target.value)}
                 />
                 
                 <Input 
                     placeholder="Senha"
                     type="text"
                     icon={FiLock}
+                    onChange={e => setPassword(e.target.value)}
                 />
 
-                <Button title="Cadastrar" />
+                <Button title="Cadastrar" onClick={handleSingUp} />
 
                 <Link to="/">
                     Voltar para login
